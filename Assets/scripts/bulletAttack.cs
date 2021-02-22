12c6c23e86aee49fb038f8e1bf19d4010beaa338
bmjohnheath <bmjohnheath@gmail.com>
2021-02-22 12:29:12 -0500

    created prefab
    startined work on bullet

---
+++ Assets/scripts/bulletAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D onbullet;
+    public float speed;
+    void Start()
+    {
+        onbullet = GetComponent<Rigidbody2D>();
+        Destroy(gameObject,5);
+        onbullet.AddForce(transform.right * speed);
+        // InvokeRepeating("destroyitself", 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // onbullet.AddForce(transform.right * speed);
+      // onbullet.velocity = transform.right * speed;
+
+    }
+     public void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/scripts/gunshoting.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunshoting : MonoBehaviour
+{
+
+    public GameObject shootpoint;
+    public GameObject bullet;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = Resources.Load("Prefab/bullet") as GameObject;
+        shootpoint = GameObject.Find("shootpointPlayer");
+    }
+    public void Atk()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+
+        }
+    }
+}
